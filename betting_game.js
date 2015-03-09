48151b6182fdd957eb019a771f2a80f0f329015f
heatherjang <heather.m.jang@gmail.com>
2015-03-09 23:57:23 +0000

    Changed min/max bets/guesses to constants

--- betting_game.js
+++ betting_game.js
@@ -1,34 +1,42 @@
-jQuery(document).ready()
+// define function, then immediately invoke it
 
-
+var MIN_BET = 5;
+var MAX_BET = 500;
+var MIN_GUESS = 1;
+var MAX_GUESS = 10;
 
 var Game = {
   play: function() {
-    var moneyRemaining = 100;
-    while (moneyRemaining > 0) {
-      alert("You have $" + moneyRemaining);
+    while (Player.money >= MIN_BET) {
+      alert("You have $" + Player.money);
       var bet = Player.placeBet();
-      // console.log("Bet: " + Bet);
+      if (!bet) {
+        continue;
+      }
+      // console.log("Bet: " + bet);
       var randNumber = Game.generateRand();
-      // console.log("Rand: " + RandNumber);
+      // console.log("Rand: " + randNumber);
       var playerGuess = Player.chooseNumber();
-      // console.log("Guess: " + PlayerGuess);
+      if (!playerGuess) {
+        continue;
+      }
+      // console.log("Guess: " + playerGuess);
       var result = Game.checkGuess(randNumber, playerGuess);
-      // console.log("Result: " + Result);
-      moneyRemaining += (bet * result);
-      // console.log(MoneyRemaining);
+      // console.log("Result: " + result);
+      Player.money += (bet * result);
+      // console.log(Player.money);
     }
   },
 
   generateRand: function() {
-    return Math.floor(Math.random() * 10);
+    return Math.ceil(Math.random() * 10);
   },
 
   checkGuess: function(rand_num, guess) {
-    if (guess == rand_num) {
+    if (guess === rand_num) {
       alert("You were correct!");
       return 2;
-    } else if (Math.abs(guess - rand_num) == 1) {
+    } else if (Math.abs(guess - rand_num) === 1) {
       alert("Close, but not quite.");
       return 0;
     } else {
@@ -39,16 +47,26 @@ var Game = {
 
 }
 
+
 var Player = {
 
+  money: 100,
+
   placeBet: function() {
-    var betAmount = prompt("How much do you want to bet?:");
-    return betAmount
+    var betAmount = prompt("Place your bet (between $5-10):");
+    if (betAmount < MIN_BET || betAmount > MAX_BET) {
+      alert("Invalid bet!");
+      return false;
+    }
+    return betAmount;
   },
 
   chooseNumber: function() {
-    var number = prompt("Choose a number between 1-10:");
-    return number;
+    var guess = prompt("Choose a number between 1-10:");
+    if (guess < MIN_GUESS || guess > MAX_GUESS) {
+      return false;
+    }
+    return guess;
   },
 
 }
