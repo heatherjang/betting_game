85f7868d728c1cbe9121170ae869f640e7c507b1
heatherjang <heather.m.jang@gmail.com>
2015-03-10 02:48:00 +0000

    Refactored Game functions

--- betting_game.js
+++ betting_game.js
@@ -6,7 +6,6 @@ $(function() {
   var MAX_GUESS = 10;
 
   var Player = {
-
     money: 100,
 
     placeBet: function(amount) {
@@ -14,11 +13,14 @@ $(function() {
       if (betAmount < MIN_BET || betAmount > MAX_BET) {
         alert('Invalid bet!');
         return false;
+      } else if (betAmount > Player.money) {
+        alert("You can't bet more than you have!")
+        return false;
       }
       return betAmount;
     },
 
-    chooseNumber: function(guess) {
+    guessNumber: function(guess) {
       var number = parseInt(guess, 10) || 0;
       if (number < MIN_GUESS || number > MAX_GUESS) {
         alert('Invalid guess! Guess must be a number between 1-10');
@@ -26,15 +28,11 @@ $(function() {
       }
       return number;
     },
-
-  }
-  // Displays available money before game start
-  $('#player-money').val(Player.money);
+  }  
 
   var Game = {
-
     play: function() {
-      var betAmount, randNumber, playerGuess, result;
+      var betAmount, playerGuess, randNumber;
       if (Player.money < MIN_BET) {
         alert('Game over. No more money left!');
         return;
@@ -43,47 +41,36 @@ $(function() {
       if (!betAmount) {
         return;
       }
-      console.log("Bet: " + betAmount);
-
-      randNumber = Game.generateRand();
-      console.log("Rand: " + randNumber);
-
-      playerGuess = Player.chooseNumber($('#player-guess').val());
+      playerGuess = Player.guessNumber($('#player-guess').val());
       if (!playerGuess) {
         return;
       }
-      console.log("Guess: " + playerGuess);
-
+      randNumber = (Math.ceil(Math.random() * MAX_GUESS));
       $('#random-number').val(randNumber);
 
-      result = Game.checkGuess(randNumber, playerGuess);
-      console.log("Result: " + result);
-      
-      Player.money += (betAmount * result);
-      console.log(Player.money);
+      Game.evaluateGuess(randNumber, betAmount, playerGuess);
+
       $('#player-money').val(Player.money);
-
     },
 
-    generateRand: function() {
-      return Math.ceil(Math.random() * MAX_GUESS);
-    },
-
-    checkGuess: function(rand_num, guess) {
-      if (guess == rand_num) {
-        alert("You were correct!");
-        return 2;
-      } else if (Math.abs(guess - rand_num) == 1) {
-        alert("Close, but not quite.");
-        return 0;
+    evaluateGuess: function(randNum, bet, guess) {
+      if (guess === randNum) {
+        alert("Nailed it!");
+        Player.money += bet;
+        return Player.money;
+      } else if (Math.abs(guess - randNum) === 1) {
+        alert("Close, but no cigar");
+        return Player.money;
       } else {
-        alert("You lose");
-        return -1;
+        alert("Wrong. You lose.");
+        Player.money -= bet;
+        return Player.money;
       }
     },
-
   }
 
+  // Display available money before game start
+  $('#player-money').val(Player.money);
+  // Run the program
   $('#start-game').click(Game.play);
-
 });
